bccd6ceb3d4db4bfd7e4a87fb31406f0367a9b46
zubene <zubene1013@gmail.com>
2026-06-26 17:30:18 +0900

    chore: 초기 폴더 구조 및 schema.sql 추가

--- network/db/schema.sql
+++ network/db/schema.sql
@@ -1,25 +1,27 @@
 create database findb;
 use findb;
 
-CREATE TABLE users (
+CREATE TABLE user_info (
     user_id VARCHAR(20) PRIMARY KEY,
-    name VARCHAR(20) NOT NULL,
-    password VARCHAR(255) NOT NULL
+    user_nm VARCHAR(20) NOT NULL,
+    pwd VARCHAR(255) NOT NULL
 );
 
-CREATE TABLE accounts (
-    account_id VARCHAR(30) PRIMARY KEY,
+CREATE TABLE accounts_info (
+    account_no VARCHAR(30) PRIMARY KEY,
     user_id VARCHAR(20) NOT NULL,
     balance INT DEFAULT 0,
-    alias VARCHAR(30),
-    is_active BOOLEAN DEFAULT TRUE
+    account_nm VARCHAR(30) NOT NULL,
+    account_stat VARCHAR(20) NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES user_info (user_id)
 );
 
-CREATE TABLE trans (
-    trans_id INT PRIMARY KEY AUTO_INCREMENT,
-    from_account VARCHAR(30) NOT NULL,   -- 출금계좌
-    to_account VARCHAR(30) NOT NULL,     -- 입금계좌
-    amount INT NOT NULL,
-    status VARCHAR(10) NOT NULL, -- 성공/실패
-    trans_time DATETIME DEFAULT CURRENT_TIMESTAMP
+CREATE TABLE transaction_history (
+    tran_id INT PRIMARY KEY AUTO_INCREMENT,
+    frm_account_no VARCHAR(30) NOT NULL,   -- 출금계좌
+    to_account_no VARCHAR(30) NOT NULL,     -- 입금계좌
+    tran_amt INT NOT NULL,
+    tran_dt DATETIME DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (frm_account_no) REFERENCES accounts_info (account_no),
+	FOREIGN KEY (to_account_no) REFERENCES accounts_info (account_no)
 );

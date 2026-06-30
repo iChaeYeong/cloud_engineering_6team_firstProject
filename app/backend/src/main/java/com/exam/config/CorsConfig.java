abdf8cf0b27ab59fb879b1105fde962df218f53b
zubene <zubene1013@gmail.com>
2026-06-30 15:36:29 +0900

    feat: add ingress to kustomize overlays, add CORS origins for bank domain

--- app/backend/src/main/java/com/exam/config/CorsConfig.java
+++ app/backend/src/main/java/com/exam/config/CorsConfig.java
@@ -16,13 +16,12 @@ public class CorsConfig {
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowCredentials(true);
         config.addAllowedOrigin("http://localhost:3000");
-	config.addAllowedOrigin("http://localhost:3000");
-	config.addAllowedOrigin("http://172.16.8.11:30081");
-	config.addAllowedOrigin("http://172.16.8.12:30081");
-	config.addAllowedOrigin("http://172.16.8.11:30080");
-	config.addAllowedOrigin("http://172.16.8.12:30080");
-	config.addAllowedOrigin("http://bank-dev.local:31508");
-	config.addAllowedOrigin("http://bank-prod.local:31508");
+        config.addAllowedOrigin("http://172.16.8.11:30081");
+        config.addAllowedOrigin("http://172.16.8.12:30081");
+        config.addAllowedOrigin("http://172.16.8.11:30080");
+        config.addAllowedOrigin("http://172.16.8.12:30080");
+        config.addAllowedOrigin("http://bank-dev.local:31508");
+        config.addAllowedOrigin("http://bank-prod.local:31508");
         config.addAllowedHeader("*");
         config.addAllowedMethod("*");
 
